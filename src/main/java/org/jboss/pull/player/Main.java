1ad4f99c115c468f173ffc550ca18ec2b0d1ab77
Jason T. Greene <jason.greene@redhat.com>
2012-09-06 23:01:08 -0500

    Add support for pagination, statuses, and go with 1.3

--- src/main/java/org/jboss/pull/player/Main.java
+++ src/main/java/org/jboss/pull/player/Main.java
@@ -5,7 +5,6 @@
  */
 package org.jboss.pull.player;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
@@ -45,6 +44,10 @@ public class Main {
     private static String GITHUB_API_JBOSS_REPO_URL;
     private static String GITHUB_LOGIN;
     private static String GITHUB_TOKEN;
+    private static String PUBLISH_PUBLIC_JOB_URL;
+    private static String COMMENT_PRIVATE_LINK = "Private: " + PUBLISH_JOB_URL + "/" + JENKINS_JOB_NAME + "/";
+    private static String COMMENT_PUBLIC_LINK = "Public: " + PUBLISH_JOB_URL + "/" + JENKINS_JOB_NAME + "/";
+
 
     static {
         Properties props;
@@ -54,6 +57,7 @@ public class Main {
             BASE_HOST = Util.require(props, "jenkins.host");
             BASE_PORT = Util.require(props, "jenkins.port");
             PUBLISH_JOB_URL = Util.require(props, "jenkins.publish.url");
+            PUBLISH_PUBLIC_JOB_URL = Util.require(props, "jenkins.publish.public.url");
             JENKINS_JOB_NAME = Util.require(props, "jenkins.job.name");
             JENKINS_JOB_TOKEN = Util.require(props, "jenkins.job.token");
             GITHUB_LOGIN = Util.require(props, "github.login");
@@ -62,6 +66,9 @@ public class Main {
             GITHUB_API_JBOSS_REPO_URL = GITHUB_API_URL + "/repos/" + GITHUB_REPO;
             BASE_URL = "http://" + BASE_HOST + ":" + BASE_PORT + "/jenkins";
             BASE_JOB_URL = BASE_URL + "/job";
+            COMMENT_PRIVATE_LINK = ":\\n Private:" + PUBLISH_JOB_URL + "/" + JENKINS_JOB_NAME + "/";
+            COMMENT_PUBLIC_LINK = ":\\n Public:" + PUBLISH_PUBLIC_JOB_URL + "/" + JENKINS_JOB_NAME + "/";
+
         } catch (Exception e) {
             e.printStackTrace(System.err);
             System.exit(1);
@@ -71,16 +78,32 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting at: " + getTime());
-
-        URL url = new URL(GITHUB_API_JBOSS_REPO_URL + "/pulls?state=open");
-        InputStream stream = url.openStream();
-        ModelNode node = ModelNode.fromJSONStream(stream);
-        stream.close();
-
         UserList whiteList = UserList.loadUserList("white-list");
         UserList adminList = UserList.loadUserList("admin-list");
 
-        for (ModelNode pull : node.asList()) {
+        for (int page = 1; ; page++) {
+            URL url = new URL(GITHUB_API_JBOSS_REPO_URL + "/pulls?state=open&page=" + page);
+            InputStream stream = url.openStream();
+            ModelNode node = ModelNode.fromJSONStream(stream);
+            stream.close();
+
+            List<ModelNode> nodes = node.asList();
+            if (nodes.size() == 0)
+                break;
+
+            System.out.println(nodes.size());
+
+            processPulls(whiteList, adminList, nodes);
+            if (page > PAGE_LIMIT) {
+                throw new IllegalStateException("Exceeded page limit");
+            }
+        }
+
+        System.out.println("Completed at: " + getTime());
+    }
+
+    private static void processPulls(UserList whiteList, UserList adminList, List<ModelNode> nodes) {
+        for (ModelNode pull : nodes) {
             int pullNumber = pull.get("number").asInt();
 
             String user = pull.get("user", "login").asString();
@@ -162,8 +185,6 @@ public class Main {
                 System.out.println("Pending build, skipping: " + pullNumber);
             }
         }
-
-        System.out.println("Completed at: " + getTime());
     }
 
     private static boolean verifyWhitelist(UserList whiteList, String user, int pullNumber, boolean notify) {
@@ -189,22 +210,35 @@ public class Main {
     }
 
     private static void notifyBuildCompleted(String sha1, String branch, int pull, int buildNumber, String status) {
-        String comment = "Build " + buildNumber + " outcome was " + status + " using a merge of " + sha1 + " on branch " + branch;
-        comment += ":\\n " + PUBLISH_JOB_URL + "/" + JENKINS_JOB_NAME +"/" + buildNumber;
+        String comment = "Build " + buildNumber + " outcome was " + status + " using a merge of " + sha1 + " on branch " + branch + ":\\n";
+        comment += COMMENT_PRIVATE_LINK + buildNumber + "\\n";
+        comment += COMMENT_PUBLIC_LINK + buildNumber + "\\n";
 
         postComment(pull, comment);
+        postStatus(buildNumber, convertJenkinsStatus(status), sha1);
     }
 
-     private static void notifyBuildRunning(String sha1, String branch,  int pull, int buildNumber) {
-        String comment = "Build " + buildNumber + " is now running using a merge of " + sha1 + " on branch " + branch;
-        comment += ":\\n " + PUBLISH_JOB_URL + "/" + JENKINS_JOB_NAME +"/" + buildNumber;
+    private static String convertJenkinsStatus(String status) {
+        if (status.equalsIgnoreCase("UNSTABLE") || status.equalsIgnoreCase("FAILURE")) {
+            return "failure";
+        } else if (status.equalsIgnoreCase("SUCCESS")) {
+            return "success";
+        }
+
+        return "error";
+    }
+
+    private static void notifyBuildRunning(String sha1, String branch,  int pull, int buildNumber) {
+        String comment = "Build " + buildNumber + " is now running using a merge of " + sha1 + " on branch " + branch + ":\\n";
+        comment += COMMENT_PRIVATE_LINK + buildNumber + "\\n";
 
         postComment(pull, comment);
+        postStatus(buildNumber, "pending", sha1);
     }
 
     private static void notifyBuildTriggered(String sha1, String branch, int pull) {
-        String comment = "Triggering build using a merge of " + sha1 + " on branch " + branch;
-        comment += ":\\n " + PUBLISH_JOB_URL + "/" + "as7-param-pull/";
+        String comment = "Triggering build using a merge of " + sha1 + " on branch " + branch +  ":\\n";;
+        comment += COMMENT_PRIVATE_LINK +"\\n";
 
         postComment(pull, comment);
     }
@@ -270,6 +304,35 @@ public class Main {
         }
 
         return comments;
+    }
+
+    private static void postStatus(int number, String status, String sha) {
+        System.out.println("Setting status: " + status + " on sha " + sha);
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(GITHUB_API_JBOSS_REPO_URL + "/statuses/" + sha);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Authorization", "token " + GITHUB_TOKEN);
+            urlConnection.setDoOutput(true);
+            urlConnection.connect();
+            PrintWriter printWriter = new PrintWriter(urlConnection.getOutputStream());
+            printWriter.println("{\"state\": \"" + status + "\", \"target_url\": \"" + PUBLISH_PUBLIC_JOB_URL + "/" + JENKINS_JOB_NAME + "/" + number + "\"}");
+            printWriter.close();
+            if (urlConnection.getResponseCode() < 200 || urlConnection.getResponseCode() > 299) {
+                System.err.printf("Problem [%d] posting a status build for sha: %s\n", urlConnection.getResponseCode(), sha);
+                Util.dumpInputStream(urlConnection.getErrorStream());
+            }
+
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            try {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            } catch (Throwable t) {
+            }
+        }
     }
 
     private static void postComment(int number, String comment) {
